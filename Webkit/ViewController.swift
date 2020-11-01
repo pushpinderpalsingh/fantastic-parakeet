27e3294e56de513730df4a97f86dc701edc3d452
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-11-01 21:21:48 +0530

    Removed Unwanted code

--- Webkit/ViewController.swift
+++ Webkit/ViewController.swift
@@ -16,16 +16,7 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
         let myURL = URL(string:"https://www.google.com")
         let myRequest = URLRequest(url: myURL!)
         
-        
-        guard let scriptPath = Bundle.main.path(forResource: "script", ofType: "js"),
-              let scriptSource = try? String(contentsOfFile: scriptPath) else { return }
-        
-        
         let config = WKWebViewConfiguration()
-        let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
-        
-        config.userContentController.addUserScript(script)
-        config.userContentController.add(self, name: "count")
         
         let webView = WKWebView(frame: view.bounds, configuration: config)
         
@@ -44,15 +35,6 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         print(message.body)
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        guard let cssPath = Bundle.main.path(forResource: "style", ofType: "css"),
-              let cssSource = try? String(contentsOfFile: cssPath).components(separatedBy: .newlines).joined() else { return }
-        
-        let js = "var style = document.createElement('style'); style.innerHTML = '\(cssSource)'; document.head.appendChild(style);"
-        
-        webView.evaluateJavaScript(js)
     }
     
 }
