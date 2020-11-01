66acd503483f365b132a1f3f8cf57d6a4539df52
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-11-01 21:13:19 +0530

    Task 01

--- Webkit/ViewController.swift
+++ Webkit/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myURL = URL(string:"https://www.google.com")
+        let myRequest = URLRequest(url: myURL!)
+        
+        
+        guard let scriptPath = Bundle.main.path(forResource: "script", ofType: "js"),
+              let scriptSource = try? String(contentsOfFile: scriptPath) else { return }
+        
+        
+        let config = WKWebViewConfiguration()
+        let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        
+        config.userContentController.addUserScript(script)
+        config.userContentController.add(self, name: "count")
+        
+        let webView = WKWebView(frame: view.bounds, configuration: config)
+        
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+                webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+                webView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+                webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        webView.load(myRequest)
+        
     }
-
-
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print(message.body)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        guard let cssPath = Bundle.main.path(forResource: "style", ofType: "css"),
+              let cssSource = try? String(contentsOfFile: cssPath).components(separatedBy: .newlines).joined() else { return }
+        
+        let js = "var style = document.createElement('style'); style.innerHTML = '\(cssSource)'; document.head.appendChild(style);"
+        
+        webView.evaluateJavaScript(js)
+    }
+    
 }
-
